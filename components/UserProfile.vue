0c5b487adea709bb875a6336cea54f8c995e5101
Jimena <j.miguez@airlst.com>
2024-01-08 15:21:32 +0100

    Move user profile to a component

---
+++ components/UserProfile.vue
@@ -0,0 +1,55 @@
+<template>
+  <section v-if="loading">Loading...</section>
+  <section v-else-if="error">{{ error }}</section>
+  <section v-if="user">
+    <hgroup>
+      <h1>{{ user.name }}</h1>
+      <h2>@{{ user.login }}</h2>
+      <p>Joined {{ joinedDate }}</p>
+    </hgroup>
+    <img :src="user.avatar_url" :alt="`Avatar for ${user.login}`" width="100" />
+    <ul>
+      <li v-for="(item, index) in bioItems" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <ul>
+      <li>{{ user.public_repos }} Repositories</li>
+      <li>{{ user.followers }} Followers</li>
+      <li>{{ user.following }} Following</li>
+    </ul>
+    <ul>
+      <li v-if="user.location">Location: {{ user.location }}</li>
+      <li v-if="user.twitter_username">Twitter: {{ user.twitter_username }}</li>
+      <li v-if="user.blog">Blog: {{ user.blog }}</li>
+      <li v-if="user.company">Company: {{ user.company }}</li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  user: Object,
+  loading: Boolean,
+  error: String,
+})
+
+// formats date
+const joinedDate = computed(() => {
+  return props.user && props.user.created_at
+    ? new Date(props.user.created_at).toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+      })
+    : ''
+})
+
+// Split the bio into an array of items
+const bioItems = computed(() => {
+  // Check if bio is not null and is a string before splitting. If bio is null, return an empty array to avoid errors
+  return props.user && props.user.bio && typeof props.user.bio === 'string'
+    ? props.user.bio.split('\r\n').filter(Boolean)
+    : []
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,27 @@
 <template>
   <section>
     <h1>GitHub Users</h1>
-
     <input
       v-model="searchUsername"
       placeholder="Enter GitHub username"
       @input="refresh"
     />
-
-    <article>
-      <div v-if="pending">Loading...</div>
-      <div v-else-if="apiError">{{ apiError }}</div>
-      <div v-else-if="user">
-        <hgroup>
-          <h1>{{ user.name }}</h1>
-          <h2>@{{ user.login }}</h2>
-          <p>Joined {{ formattedDate }}</p>
-        </hgroup>
-        <img
-          :src="user.avatar_url"
-          :alt="`Avatar for ${user.login}`"
-          width="100"
-        />
-        <ul>
-          <li v-for="(item, index) in bioItems" :key="index">
-            {{ item }}
-          </li>
-        </ul>
-        <ul>
-          <li>{{ user.public_repos }}</li>
-          <li>{{ user.followers }}</li>
-          <li>{{ user.following }}</li>
-        </ul>
-        <ul>
-          <li>Location: {{ user.location }}</li>
-          <li>Twitter: {{ user.twitter_username }}</li>
-          <li>blog: {{ user.blog }}</li>
-          <li>company: {{ user.company }}</li>
-        </ul>
-      </div>
-    </article>
+    <UserProfile :user="user" :loading="pending" :error="apiError" />
   </section>
 </template>
+
 <script setup>
-const searchUsername = ref('octocat') // Start with a default username
+const searchUsername = ref('octocat')
 const { user, pending, error, refresh, userNotFound } =
   useGithubUsers(searchUsername)
-// Checks type of error from API
+
+// Computes type of error from API
 const apiError = computed(() => {
-  if (error.value) {
-    return error.value.response && error.value.response.status === 404
-      ? 'No results'
-      : 'An error occurred.'
+  if (userNotFound.value) {
+    return 'No results'
+  } else if (error.value) {
+    return 'An error occurred.'
   }
   return null
-})
-// formats date
-const formattedDate = computed(() => {
-  const date = new Date(user.value.created_at)
-  return date
-    .toLocaleDateString('en-GB', {
-      day: '2-digit',
-      month: 'short',
-      year: 'numeric',
-    })
-    .replace(/ /g, ' ')
-})
-
-// Split the bio into an array of items
-const bioItems = computed(() => {
-  // Check if bio is not null and is a string before splitting
-  if (user.value.bio && typeof user.value.bio === 'string') {
-    return user.value.bio.split('\r\n').filter(Boolean) // This filters out any empty strings
-  }
-  return [] // If bio is null, return an empty array to avoid errors
 })
 </script>
