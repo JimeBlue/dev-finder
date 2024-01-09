a39aa9b6396d5ddf1296aafb2164de075894cca2
Jimena <j.miguez@airlst.com>
2024-01-09 14:58:09 +0100

    Add toggle button for light/dark mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
-    <button @click="toggleDarkMode">Toggle Dark Mode</button>
+    <button @click="toggleDarkMode">
+      <span v-if="!darkMode" class="inline-flex"
+        ><span>light</span> <IconsSun class="w-8 h-8 text-red-500"
+      /></span>
+      <span v-if="darkMode" class="inline-flex"
+        ><span>dark</span> <IconsMoon class="w-8 h-8 text-red-500"
+      /></span>
+    </button>
     <h1 class="text-blue-500 dark:text-slate-900">GitHub Users</h1>
     <form @submit.prevent="triggerSearch">
       <input v-model="searchUsername" placeholder="Enter GitHub username" />
@@ -11,7 +18,7 @@
 </template>
 
 <script setup>
-const { toggleDarkMode } = useDarkMode()
+const { darkMode, toggleDarkMode } = useDarkMode()
 const { user, pending, error, searchUser, userNotFound } =
   useGithubUsers('JimeBlue')
 
