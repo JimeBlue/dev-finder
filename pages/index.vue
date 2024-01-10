3d86a4bd6eb449f94f88d162936d56edcd4d8893
Jimena <j.miguez@airlst.com>
2024-01-10 16:36:01 +0100

    Add styled header to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,25 @@
 <template>
   <section class="text-blue-500 dark:text-white">
-    <button
-      @click="toggleDarkMode"
-      class="flex space-x-4 items-center text-sm transition duration-300"
-      :class="
-        darkMode
-          ? 'text-white hover:text-blue-gray-300'
-          : 'text-blue-gray-400 hover:text-blue-gray-800'
-      "
-    >
-      <span>
-        {{ darkMode ? 'LIGHT' : 'DARK' }}
-      </span>
-      <IconsMoon v-if="darkMode" />
-      <IconsSun v-else />
-    </button>
-    <h1 class="text-blue-500 dark:text-slate-900">GitHub Users</h1>
+    <header class="flex justify-between">
+      <h1 class="text-2xl text-blue-gray-800 dark:text-white font-bold">
+        devfinder
+      </h1>
+      <button
+        @click="toggleDarkMode"
+        class="flex space-x-4 items-center text-sm transition duration-300"
+        :class="
+          darkMode
+            ? 'text-white hover:text-blue-gray-300'
+            : 'text-blue-gray-400 hover:text-blue-gray-800'
+        "
+      >
+        <span>
+          {{ darkMode ? 'LIGHT' : 'DARK' }}
+        </span>
+        <IconsMoon v-if="darkMode" />
+        <IconsSun v-else />
+      </button>
+    </header>
     <form @submit.prevent="triggerSearch">
       <input v-model="searchUsername" placeholder="Enter GitHub username" />
       <button type="submit" class="btn btn-primary">Search</button>
